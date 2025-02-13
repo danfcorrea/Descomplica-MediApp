f849a8f6db5cf53e555924c60a863086dc94c13e
Daniel Corrêa <daniel.1997.df@gmail.com>
2025-02-13 10:38:05 -0300

    feat: controller route mapping

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,8 @@
 import { express } from "express"
+import appointmentController from "./AppointmentController.js"
+import doctorController from "./DoctorController.js"
+import pacientController from "./PacientController.js"
+import prescriptionController from "./PrescriptionController.js"
 
 const router = express.Router();
 
@@ -7,5 +11,10 @@ router.get(
         res.status(200).json({message: ""});
     }
 );
+
+router.use("/", appointmentController);
+router.use("/", doctorController);
+router.use("/", pacientController);
+router.use("/", prescriptionController);
 
 export default router
